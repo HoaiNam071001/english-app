4108aaa829009981daabf1082ddc9440d73e5c32
Hoai Nam <v.namnh141@vinsmartfuture.tech>
2026-01-12 09:05:36 +0700

    fix css

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -95,7 +95,7 @@ export const MainHeader = () => {
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center justify-between mx-auto max-w-8xl">
+      <div className="container flex h-14 items-center justify-between mx-auto max-w-8xl px-4">
         <div className="mr-4 hidden md:flex">
           <Link to={ROUTES.HOME} className="mr-6 flex items-center space-x-2">
             <span className="hidden font-bold sm:inline-block text-lg bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-cyan-500">
