aab6f05948ada84c96362c3488c1e315d3997cf8
HoaiNam0710 <namnguyen@busmap.vn>
2026-01-17 12:27:58 +0700

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
       workbox: {
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
+      devOptions: {
+        enabled: true, // Bật cái này để test PWA ngay ở localhost (npm run dev)
+      },
     }),
   ],
   resolve: {
